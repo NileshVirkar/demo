417ff4eef715f2729c36507e2449e42100224055
Nilesh Virkar <nilesh.virkar@acellere.com>
2021-06-01 15:15:26 +0530

    Update App.java

--- App.java
+++ App.java
@@ -10,10 +10,6 @@ public class App
 	
     public static void main( String[] args )
     {
-    	try {	
-    	} catch (Exception e) {
-		}
-    	
         System.out.println( "Hello World!" );
     }
 }
